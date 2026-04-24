2ff482e0d3ca38af9c15fa1b2987d3761ac0950d
KevinKuslin <tyrantnitrox@gmail.com>
2026-04-24 14:48:42 +0700

    Commit sign in changes

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -8,14 +8,19 @@
 </head>
 <body class=" dark:bg-[#0a0a0a] text-[#1b1b18] flex items-center justify-center flex-col">
     <div class="w-full min-h-screen grid place-items-center bg-green-800 p-10">
-        <div class="w-full h-full flex rounded-[20px] bg-white shadow opacity-90">
-            <div class="col-6 bg-amber-800 flex items-center justify-center w-full">
+
+        <div class="w-full h-full flex flex-col rounded-[20px] bg-white shadow opacity-90">
+            <div class="h-full flex bg-amber-500 items-center justify-center w-full">
+                <div class="bg-blue-500 h-full row-5"></div>
+                <div class="bg-green-500 h-full row-7">
+                    
+                </div>
+            </div>
+            <div class="h-full bg-amber-200 flex items-center justify-center w-full">
                 
             </div>
-            <div class="col-6 bg-amber-200 flex items-center justify-center w-full">
-
-            </div>
         </div>
+
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return view('landing');
 });
 
-Route::get('/signin', function() {
+Route::get('/sign-in', function() {
     return view('signin'); 
 })->name('signin.view'); 
