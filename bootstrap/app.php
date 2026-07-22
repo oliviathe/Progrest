25e195b0247ea227212e572ab741a4c1bea65942
Jonea <joneakristiawan@gmail.com>
2026-07-22 08:16:56 +0700

    Fix: setLocale

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append:[
-            \App\Http\Middleware\setLocale::class
+            \App\Http\Middleware\SetLocale::class
         ]); 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
